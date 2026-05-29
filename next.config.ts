61df0f6614fd67d0ff29c9ace75c24690fcc4bca
divear <lukyn.odehnal333@gmail.com>
2026-05-29 11:26:34 +0100

    fix limit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb", // Raises for the photo upload in the contact form
+    },
+  },
 };
 
 export default nextConfig;
